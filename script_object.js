de91e69e915db15a21ca5e354b694932009c2194
NaomiThomson <naomi.thomson@yahoo.com>
2017-04-04 23:17:05 -0700

    renamed objects 'variables' and 'functions'

--- script_object.js
+++ script_object.js
@@ -1,4 +1,4 @@
-var variables = {
+var gameInfo = {
 	"names": ["gascoigne", "ebrietas", "micolash", "amygdalan", "yharnam", "byrgenwerth"],
 	"nameArray": [],
 	"wins": 0,
@@ -8,7 +8,7 @@ var variables = {
 	"lettersGuessed": []
 };
 
-var functions = {
+var gameMethods = {
 	getLength: function(objArray) {
 		var length = Object.keys(objArray).length;
 		return length;
@@ -39,13 +39,13 @@ var functions = {
 };
 
 // before game starts, choose word randomly from list and display its letters as underscores
-var word_list_length = functions.getLength(variables.names);
-var word = functions.genRanWord(variables.names, word_list_length);
+var word_list_length = gameMethods.getLength(gameInfo.names);
+var word = gameMethods.genRanWord(gameInfo.names, word_list_length);
 var word_expanded = word.split("");
-var word_length = functions.getLength(word_expanded);
-	var unknown_word = functions.createUnderscores(variables.nameArray, word_length);
-var display_new_word = functions.arrayToStr(unknown_word, word_length, variables.newWord);
-functions.updateHTML("current-word", display_new_word);
+var word_length = gameMethods.getLength(word_expanded);
+	var unknown_word = gameMethods.createUnderscores(gameInfo.nameArray, word_length);
+var display_new_word = gameMethods.arrayToStr(unknown_word, word_length, gameInfo.newWord);
+gameMethods.updateHTML("current-word", display_new_word);
 
 // game starts onkeyup
 document.onkeyup = function(event) {
@@ -53,24 +53,24 @@ document.onkeyup = function(event) {
 
 	// for letters found inside the word, reveal letter
 	for (var i = 0; i < word_length; i++) {
-		if (functions.isInArray(word_expanded, letter) === true && word_expanded[i] === letter) {
+		if (gameMethods.isInArray(word_expanded, letter) === true && word_expanded[i] === letter) {
 			unknown_word[i] = letter;
 		};
 	};
-	display_current_word = functions.arrayToStr(unknown_word, word_length, variables.currentWord);
-	functions.updateHTML("current-word", display_current_word);
+	display_current_word = gameMethods.arrayToStr(unknown_word, word_length, gameInfo.currentWord);
+	gameMethods.updateHTML("current-word", display_current_word);
 
 	// if letter isn't in the word and has not already been guessed, subtract from tries left
-	if (functions.isInArray(word_expanded, letter) === false && functions.isInArray(variables.lettersGuessed, letter) === false) {
-		variables.tries -= 1;
-		functions.updateHTML("tries-left", variables.tries);
+	if (gameMethods.isInArray(word_expanded, letter) === false && gameMethods.isInArray(gameInfo.lettersGuessed, letter) === false) {
+		gameInfo.tries -= 1;
+		gameMethods.updateHTML("tries-left", gameInfo.tries);
 	};
 
 	// add letter to letters guessed
-	variables.lettersGuessed.push(letter);
+	gameInfo.lettersGuessed.push(letter);
 
 	// delete letter guessed from letters left
-	if (functions.isInArray(variables.lettersGuessed, letter) === true) {
+	if (gameMethods.isInArray(gameInfo.lettersGuessed, letter) === true) {
 		var delete_letter = document.getElementById(letter);
 		delete_letter.style.visibility = "hidden";
 	};
@@ -80,11 +80,12 @@ document.onkeyup = function(event) {
 		var div = document.createElement('div');
 		div.setAttribute('class', 'win-img');
 		document.getElementById('current-word').appendChild(div);
-		variables.wins += 1;
-		functions.updateHTML("score", "Wins: " + variables.wins);
+		gameInfo.wins += 1;
+		gameMethods.updateHTML("score", "Wins: " + gameInfo.wins);
+		// window.location = './index.html';
 	};
 
-	if (word != display_current_word && variables.tries == 0) {
+	if (word != display_current_word && gameInfo.tries == 0) {
 		var div = document.createElement('div');
 		div.setAttribute('class', 'lose-img');
 		document.getElementById('current-word').appendChild(div);
